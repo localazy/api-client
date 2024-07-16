7335902dd3d69ea045ae53c7b6bd64f97eec79a2
David Václavek <49518842+david-vaclavek@users.noreply.github.com>
2024-07-16 17:54:35 +0200

    LOC-1114 - Figma Fixes and Improvements (#22)
    * ✨ feat(Import): add upload status endpoint
    
    * ✨ feat(Key): add deprecate endpoint
    
    * 🐛 fix:(screenshots): fix `removeMetadata` data type
    
    * 🐛 fix(screenshots): fix `addMetadata` and `removeMetadata` params casing
    
    * ✨ feat(`listKeys`): add support for `metadata` parameter
    
    * 🚨 lint: fix
    
    ---------
    
    Co-authored-by: david-vaclavek <david@localazy.com>

---
+++ src/types/import-progress-request.ts
@@ -0,0 +1,13 @@
+import { Project } from '@/types/project';
+
+export type ImportProgressRequest = {
+  /**
+   * Project object or Project ID.
+   */
+  project: Project | string;
+
+  /**
+   * Import session identifier.
+   */
+  importBatch: string;
+};
---
+++ src/types/upload-session-status.ts
@@ -0,0 +1,23 @@
+import { UploadStatus } from '@/enums/upload-status';
+
+export type UploadSessionStatus = {
+  /**
+   * Possible status of the upload session.
+   */
+  status: UploadStatus;
+
+  /**
+   * Number of keys added in the upload session.
+   */
+  added?: number;
+
+  /**
+   * Number of keys updated in the upload session.
+   */
+  updated?: number;
+
+  /**
+   * Number of keys deprecated in the upload session.
+   */
+  deprecated?: number;
+};
